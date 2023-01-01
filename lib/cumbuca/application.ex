7f8c0507276e99f6e12360922fa684ec7d8ed8b4
josue santos <josue.santos.dev@gmail.com>
2023-01-01 16:02:53 -0300

    (config) config Plug

--- lib/cumbuca/application.ex
+++ lib/cumbuca/application.ex
@@ -1,6 +1,4 @@
 defmodule Cumbuca.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,13 @@ defmodule Cumbuca.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Cumbuca.Worker.start_link(arg)
-      # {Cumbuca.Worker, arg}
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: Cumbuca.Endpoint,
+        options: [port: 3030]
+      )
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Cumbuca.Supervisor]
     Supervisor.start_link(children, opts)
   end
