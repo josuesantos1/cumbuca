28d57ca6ae49257a3b19affdb93f2c53dae63309
Josue santos <josue.santos.dev@gmail.com>
2024-07-21 12:21:16 -0300

    config(database) config datomic

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [metosin/reitit "0.7.1"]
                  [metosin/ring-swagger-ui "5.9.0"]
                  [com.fbeyer/init "0.2.96"]
-                 [aero/aero "1.1.6"]]
+                 [aero/aero "1.1.6"]
+                 [com.datomic/peer "1.0.7180"]]
   :main ^:skip-aot cumbuca.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/cumbuca/config/config.clj
+++ src/cumbuca/config/config.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.io :as io]
             [aero.core :as aero]))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn load-config
   {:init/name ::config
    :init/tags [:app/config]}
