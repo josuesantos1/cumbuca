0a5767835709c9a352524bbc24f5499fa376059f
Josue santos <josue.santos.dev@gmail.com>
2024-07-21 01:55:54 -0300

    start project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cumbuca
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cumbuca/core_test.clj
@@ -0,0 +1,7 @@
+(ns cumbuca.core-test
+  (:require [clojure.test :refer :all]
+            [cumbuca.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
