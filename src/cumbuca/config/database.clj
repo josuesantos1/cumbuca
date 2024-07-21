515782b9ad5d295ed18d5e8df2a64a1339fe1578
Josue santos <josue.santos.dev@gmail.com>
2024-07-21 15:08:37 -0300

    config(database): create coerce schema

--- src/cumbuca/config/database.clj
+++ src/cumbuca/config/database.clj
@@ -1,9 +1,41 @@
 (ns cumbuca.config.database
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [cumbuca.contracts.schemas.customer :refer [customer]]))
 
-(defn database
+(def ^:private datomic-schemas
+  (->> [customer]
+       (map rest)))
+
+(defn create-schema
+  [[field type]]
+  {:db/ident       field
+   :db/unique      :db.unique/identity
+   :db/valueType   type
+   :db/cardinality :db.cardinality/one})
+
+(defn coerce-type
+  [[field type]]
+  (cond (= type string?) [field :db.type/string]
+        (= type int?) [field :db.type/long]))
+
+(defn coerce-types
+  [field]
+  (->> field
+       (map coerce-type)
+       (map create-schema)))
+
+(defn- create-datomic-schema
+  [datomic]
+  (->> datomic-schemas
+       (map coerce-types)
+       flatten
+       (d/transact datomic)))
+
+(defn datomic
   {:init/tags [:datomic/connect]
    :init/inject [[:get :app/config :datomic-uri]]}
   [uri]
-  (d/create-database uri)
-  (d/connect uri))
+  (let [_ (d/create-database uri)
+        datomic (d/connect uri)]
+    (create-datomic-schema datomic)
+    datomic))
